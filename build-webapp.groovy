56fa720a5a0a26bbb023d989386f4cee77167c0e
Owen VC_LAST_NAME <owen@vividcortex.com>
2014-03-16 00:32:05 -0400

    [minor]

--- build-webapp.groovy
+++ build-webapp.groovy
@@ -1,7 +1,21 @@
 job {
     name 'build-webapp'
 
+    stringParam("BRANCH")
+
     scm {
+        name('origin')
         git('git@github.com:VividCortex/webapp.git')
+        branch('origin/$BRANCH')
+    }
+}
+
+job {
+    name 'release-webapp'
+
+    scm {
+        origin('origin')
+        git('git@github.com:VividCortex/ansible-playbook.git')
+        branch('origin/master')
     }
 }
