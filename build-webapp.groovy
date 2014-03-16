26433eeb85c748b119c20ab6ee8dcd461f60f5d6
Owen VC_LAST_NAME <owen@vividcortex.com>
2014-03-16 01:31:32 -0400

    [minor]

--- build-webapp.groovy
+++ build-webapp.groovy
@@ -2,7 +2,7 @@ def project = 'webapp'
 
 job {
     name "${project}-build"
-
+    stringParam("BRANCH")
     scm {
         git('git@github.com:VividCortex/${project}.git', "origin/master")
 
@@ -19,6 +19,19 @@ job {
         git('git@github.com:VividCortex/ansible-playbook.git', "origin/master")
     }
     steps {
-        shell("echo 'Works'")
+        shell("ansible-playbook -i stage ${name}.yml")
     }
 }
+
+
+//environmentVariables {
+//    scriptFile(String filePath)
+//    script(String content)
+//    env(Object key, Object value)
+//    envs(Map<Object, Object> map)
+//    groovy(String groovyScript)
+//    propertiesFile(String filePath)
+//    loadFilesFromMaster(boolean loadFromMaster)
+//    keepSystemVariables(boolean keepSystemVariables)
+//    keepBuildVariables(boolean keepBuildVariables)
+//}
